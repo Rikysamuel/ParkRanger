91d68082c6005b4cabc4b394e23fbda6cb76aced
Edmund Ophie <edmundophie@Edmunds-MacBook-Pro.local>
2015-02-13 02:30:52 +0700

    Register is finished. Also fix the sql schema

--- parkranger.sql
+++ parkranger.sql
@@ -2,8 +2,8 @@
 -- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 10, 2015 at 09:09 AM
+-- Host: localhost
+-- Generation Time: Feb 12, 2015 at 08:28 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `park_ranger`
+-- Database: `parkranger`
 --
 
 -- --------------------------------------------------------
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `ditanggapi` (
 
 CREATE TABLE IF NOT EXISTS `member` (
   `jumlah_report` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL,
+  `status` int(11) NOT NULL,
   `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `member` (
 --
 
 CREATE TABLE IF NOT EXISTS `pengaduan` (
-  `id_laporan` int(11) NOT NULL,
+`id_laporan` int(11) NOT NULL,
   `rank_vote` int(11) NOT NULL,
   `waktu` datetime NOT NULL,
   `status` varchar(20) NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `pengaduan` (
   `pelapor` int(11) NOT NULL,
   `keterangan` text NOT NULL,
   `jenis_laporan` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -82,8 +82,8 @@ CREATE TABLE IF NOT EXISTS `pihak_berwenang` (
 CREATE TABLE IF NOT EXISTS `taman` (
   `nama` varchar(50) NOT NULL,
   `alamat` text NOT NULL,
-  `id_taman` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+`id_taman` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -92,10 +92,10 @@ CREATE TABLE IF NOT EXISTS `taman` (
 --
 
 CREATE TABLE IF NOT EXISTS `tanggapan` (
-  `id_tanggapan` int(11) NOT NULL,
+`id_tanggapan` int(11) NOT NULL,
   `keterangan` text NOT NULL,
   `id_penanggap` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -104,14 +104,14 @@ CREATE TABLE IF NOT EXISTS `tanggapan` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
+`id_user` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `email` varchar(20) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `role` int(11) NOT NULL,
+  `role` varchar(50) NOT NULL,
   `picture` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
 
 -- --------------------------------------------------------
 
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 CREATE TABLE IF NOT EXISTS `vote_laporan` (
   `id_laporan` int(11) NOT NULL,
-  `vote_by` int(11) NOT NULL
+  `voter` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -174,8 +174,32 @@ ALTER TABLE `user`
 -- Indexes for table `vote_laporan`
 --
 ALTER TABLE `vote_laporan`
- ADD PRIMARY KEY (`id_laporan`), ADD KEY `id_laporan` (`id_laporan`);
+ ADD PRIMARY KEY (`id_laporan`,`voter`), ADD KEY `voter` (`voter`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pengaduan`
+--
+ALTER TABLE `pengaduan`
+MODIFY `id_laporan` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `taman`
+--
+ALTER TABLE `taman`
+MODIFY `id_taman` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tanggapan`
+--
+ALTER TABLE `tanggapan`
+MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
 --
 -- Constraints for dumped tables
 --
@@ -184,8 +208,8 @@ ALTER TABLE `vote_laporan`
 -- Constraints for table `ditanggapi`
 --
 ALTER TABLE `ditanggapi`
-ADD CONSTRAINT `ditanggapi_ibfk_1` FOREIGN KEY (`id_tanggapan`) REFERENCES `tanggapan` (`id_tanggapan`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `ditanggapi_ibfk_2` FOREIGN KEY (`id_laporan`) REFERENCES `pengaduan` (`id_laporan`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `ditanggapi_ibfk_1` FOREIGN KEY (`id_laporan`) REFERENCES `pengaduan` (`id_laporan`),
+ADD CONSTRAINT `ditanggapi_ibfk_2` FOREIGN KEY (`id_tanggapan`) REFERENCES `tanggapan` (`id_tanggapan`);
 
 --
 -- Constraints for table `member`
@@ -197,9 +221,9 @@ ADD CONSTRAINT `member_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_us
 -- Constraints for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
-ADD CONSTRAINT `pengaduan_ibfk_1` FOREIGN KEY (`id_taman`) REFERENCES `taman` (`id_taman`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `pengaduan_ibfk_3` FOREIGN KEY (`pelapor`) REFERENCES `member` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `pengaduan_ibfk_4` FOREIGN KEY (`ditangani_by`) REFERENCES `pihak_berwenang` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `pengaduan_ibfk_4` FOREIGN KEY (`ditangani_by`) REFERENCES `pihak_berwenang` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `pengaduan_ibfk_5` FOREIGN KEY (`id_taman`) REFERENCES `taman` (`id_taman`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `pihak_berwenang`
@@ -211,13 +235,14 @@ ADD CONSTRAINT `pihak_berwenang_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user
 -- Constraints for table `tanggapan`
 --
 ALTER TABLE `tanggapan`
-ADD CONSTRAINT `tanggapan_ibfk_1` FOREIGN KEY (`id_penanggap`) REFERENCES `pihak_berwenang` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `tanggapan_ibfk_1` FOREIGN KEY (`id_penanggap`) REFERENCES `pihak_berwenang` (`id_user`);
 
 --
 -- Constraints for table `vote_laporan`
 --
 ALTER TABLE `vote_laporan`
-ADD CONSTRAINT `vote_laporan_ibfk_1` FOREIGN KEY (`id_laporan`) REFERENCES `pengaduan` (`id_laporan`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `vote_laporan_ibfk_1` FOREIGN KEY (`id_laporan`) REFERENCES `pengaduan` (`id_laporan`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `vote_laporan_ibfk_2` FOREIGN KEY (`voter`) REFERENCES `member` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
