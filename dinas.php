311ac9df796745060ed2f8e332ce607bc0da2dfc
Rikysamuel <rikz.samuel@gmail.com>
2015-02-12 18:39:33 +0700

    edit css

--- dinas.php
+++ dinas.php
@@ -68,43 +68,7 @@
 
                 //data dalam array diprint ke halaman html
                 for($it=0;$it<sizeof($row)-1;$it++){
-                  echo '<div class="panel panel-default">';
-                  echo '<div class="panel-body">
-					<div class="col-xs-12 deskripsi-wrapper">
-						<div class="col-xs-3">
-							<a href="#" class="thumbnail">
-								<img src="img/taman1.jpg" alt="'.$row[$it][4].'">
-							</a>
-						</div>
-						<div class="col-xs-9 deskripsi">
-							<h2><a href="#"><strong>'.fetchTaman($link,$row[$it][5])['nama'].'</strong></a></h2>
-							<p class="text-warning">Jenis laporan : '.$row[$it][9].'</p>
-							<p style="min-height:60px">
-							'.$row[$it][8].'</p>
-							<div class="col-xs-9 status">
-								<p>
-									<span class="text-danger"><span class="glyphicon glyphicon-remove"></span> '.$row[$it][3].'</span><br />
-									<small>Pelapor : <a href="profile.html" class="text-primary"> '.fetchPelapor($link,$row[$it][7])['nama'].' </a> <a href="#"><span class="text-danger glyphicon glyphicon-exclamation-sign"></span></a></small>
-								</p>
-							</div>
-							<div class="col-xs-3">
-								<button id="bt'.$row[$it][0].'" type="button" class="btn btn-primary btn-block" data-toggle="collapse" data-target="#post'.$row[$it][0].'" style="visibility:hidden" onclick="this.style.visibility=\'hidden\'">Tanggapi</button>
-						    </div>
-						</div>
-						</div>
-						<div id="post'.$row[$it][0].'" class="collapse in">
-							<div class="col-xs-9 komentar">
-								<textarea name="komentar" rows="3" class="form-control" placeholder="Tanggapan"></textarea>
-							</div>
-							<div class="col-xs-3 konfirmasi-btn">
-								<span>
-									<input type="submit" value="Konfirmasi" class="btn btn-block btn-primary" data-toggle="collapse" data-target="#post'.$row[$it][0].'" onclick="bt'.$row[$it][0].'.style.visibility=\'visible\'">
-								</span>
-							</div>
-
-						</div>
-	        		</div>
-	        	</div>';
+                  echo reports($link, $row,$it);
                 }
                 //tutup koneksi
                 closeConnection($link);
