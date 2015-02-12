48629cec4fe1827850ac3c47cb4d8c70e41becce
Rikysamuel <rikz.samuel@gmail.com>
2015-02-12 14:49:44 +0700

    fetch post from database

--- dinas.php
+++ dinas.php
@@ -2,7 +2,11 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> 
+<?php
+	$sort;
+?>
+<html class="no-js"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,54 +36,85 @@
 	       	</div>
 	       	<br/>
 	       	<div class="dropdown text-right">
+	       	<input type="hidden" id="temp1" name="temp1"></input>
 	       	Sort by : &nbsp;
 				<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
 					Most recent
 					<span class="caret"></span>
 				</button>
 				<ul class="dropdown-menu dropdown-menu-right" role="menu" aria-labelledby="dropdownMenu1">
-					<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Most recent</a></li>
-					<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Top votes</a></li>
-					<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Recently handled</a></li>
+					<li role="presentation" name="sort" id="recent" onclick="$('#temp1').val('0');"><a role="menuitem" tabindex="-1" href="#">Most recent</a></li>
+					<li role="presentation" name="sort" id="votes" onclick="$('#temp1').val('1');"><a role="menuitem" tabindex="-1" href="#">Top votes</a></li>
+					<li role="presentation" name="sort" id="handled" onclick="$('#temp1').val('2');"><a role="menuitem" tabindex="-1" href="#">Recently handled</a></li>
 				</ul>
 			</div>
 			<br />
-	       	<div class="panel panel-default">
-				<div class="panel-body">
+
+			<?php
+				$sort=$_POST['temp1'];
+				echo 'sort: ';
+				echo $sort;
+                $link=mysqli_connect("localhost","root","","park_ranger");
+                // Cek koneksi ke database
+                if (mysqli_connect_errno()) {
+                  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                }
+
+                $id = 0;
+
+                //ambil data ke dalam array row dari database
+                $result = mysqli_query($link,"SELECT * FROM pengaduan ORDER BY pengaduan.id_laporan DESC");
+                while($row[] = mysqli_fetch_array($result));
+
+                $res = mysqli_query($link,"SELECT nama FROM user WHERE id_user='$id'");
+				$name = $res->fetch_assoc();
+
+                //data dalam array diprint ke halaman html
+                for($it=0;$it<sizeof($row)-1;$it++){
+                  echo '<div class="panel panel-default">';
+                  echo '<div class="panel-body">
 					<div class="col-xs-12 deskripsi-wrapper">
 						<div class="col-xs-3">
 							<a href="#" class="thumbnail">
-								<img src="img/taman1.jpg" alt="GAMBAR TAMAN">
+								<img src="img/taman1.jpg" alt="'.$row[$it][4].'">
 							</a>
 						</div>
 						<div class="col-xs-9 deskripsi">
-							<h2><a href="#"><strong>NAMA TAMAN</strong></a></h2>
-							<p class="text-warning">Jenis laporan : JENIS KERUSAKAN</p>
-							<p>
-							ISI LAPORAN(substr aja)</p>
-							<div class="col-xs-9 status" style="padding: 60px 0px 0px">
+							<h2><a href="#"><strong>ID:'.$row[$it][5].'</strong></a></h2>
+							<p class="text-warning">Jenis laporan : '.$row[$it][9].'</p>
+							<p style="min-height:80px">
+							'.$row[$it][8].'</p>
+							<div class="col-xs-9 status">
 								<p>
-									<span class="text-danger"><span class="glyphicon glyphicon-remove"></span> STATUS LAPORAN</span><br />
-									<small>Pelapor : <a href="profile.html" class="text-primary">YANG MELAPOR </a> <a href="#"><span class="text-danger glyphicon glyphicon-exclamation-sign"></span></a></small>
+									<span class="text-danger"><span class="glyphicon glyphicon-remove"></span> '.$row[$it][3].'</span><br />';
+									$id=$row[$it][7];
+									$res = mysqli_query($link,"SELECT nama FROM user WHERE id_user='$id'");
+									$name = $res->fetch_assoc();
+									echo '<small>Pelapor : <a href="profile.html" class="text-primary"> '.$name['nama'].' </a> <a href="#"><span class="text-danger glyphicon glyphicon-exclamation-sign"></span></a></small>
 								</p>
 							</div>
 							<div class="col-xs-3">
-								<button id="bt1" type="button" class="btn btn-primary btn-block" data-toggle="collapse" data-target="#demo1" style="visibility:hidden" onclick="this.style.visibility='hidden'">Komentar</button>
+								<button id="bt'.$row[$it][0].'" type="button" class="btn btn-primary btn-block" data-toggle="collapse" data-target="#post'.$row[$it][0].'" style="visibility:hidden" onclick="this.style.visibility=\'hidden\'">Tanggapi</button>
 						    </div>
 						</div>
-					</div>
-					<div id="demo1" class="collapse in">
-						<div class="col-xs-9 komentar">
-							<textarea name="komentar" rows="3" class="form-control" placeholder="Komentar">ISI TANGGAPAN</textarea>
 						</div>
-						<div class="col-xs-3 konfirmasi-btn">
-							<span>
-								<input type="submit" value="Konfirmasi" class="btn btn-block btn-primary" data-toggle="collapse" data-target="#demo1" onclick="bt1.style.visibility='visible'">
-							</span>
+						<div id="post'.$row[$it][0].'" class="collapse in">
+							<div class="col-xs-9 komentar">
+								<textarea name="komentar" rows="3" class="form-control" placeholder="Tanggapan"></textarea>
+							</div>
+							<div class="col-xs-3 konfirmasi-btn">
+								<span>
+									<input type="submit" value="Konfirmasi" class="btn btn-block btn-primary" data-toggle="collapse" data-target="#post'.$row[$it][0].'" onclick="bt'.$row[$it][0].'.style.visibility=\'visible\'">
+								</span>
+							</div>
 						</div>
-					</div>
-	        	</div>
-      		</div><!-- End of Panel -->
+	        		</div>
+	        	</div>';
+                }
+                //tutup koneksi
+                mysqli_close($link);
+            ?>
+
       		<div class="panel panel-default">
 				<div class="panel-body">
 					<div class="col-xs-12 deskripsi-wrapper">
@@ -115,8 +150,9 @@
 						</div>
 					</div>
 				</div>
-        	</div>
-  		</div><!-- End of Panel -->
+  			</div><!-- End of Panel -->
+
+
 		<nav class="text-center">
 			<ul class="pagination">
 				<li class="disabled">
